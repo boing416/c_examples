e872a929785f9fd427336ecae2e168712ba092b2
Murat Nurkassymov <boing416@gmail.com>
2019-04-06 12:54:30 -0600

    SUDOKU

---
+++ Rush01/ex00/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mnurkass <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/06 08:03:00 by mnurkass          #+#    #+#             */
+/*   Updated: 2019/04/06 09:16:48 by mnurkass         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __FT_H__
+# define __FT_H__
+
+void ft_putchar(char c);
+void ft_putstr(char *str);
+int ft_strcmp(char *s1, char *s2);
+int ft_strlen(char *str);
+void ft_swap(int *a, int *b);
+
+#endif
